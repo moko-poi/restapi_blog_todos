b887d38d98e02c7a3c39bd320611b2adf053c18d
b9p31049 <b9p31049@shonan.bunkyo.ac.jp>
2021-11-14 15:20:27 +0900

    added host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-api-blogapp.herokuapp.com']
 
 
 # Application definition
